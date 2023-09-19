f98e4ca43016ede937d79d134f5c1807d6871278
Developer <136099805+Developer7861@users.noreply.github.com>
2023-09-19 12:08:52 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,6 +1,6 @@
-def call(ip,creds,warFile){
+def call(ip, creds, warFile){
   // install ssh agent plugin
-    sshagent([creds]) {
+    sshagent(['tomcat-dev']) {
      // copy war file local to tomcat -dev server
      sh "scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
      // restart the tomcat9 server
